268920c91ec60249ddbd49d189f7d52972a4bd20
MonLes <73730090+Monles@users.noreply.github.com>
2023-05-06 03:08:22 +0000

    init

---
+++ 01-variable.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int age = 18;
+    float gpa = 3.5;
+    char grade = 'A';
+    char game[] = "Elden Ring";
+
+    printf("Your age is %d\n", age);
+    printf("Your gpa is %f\n", gpa);
+    printf("Your grade is %c\n", grade);
+    printf("My favorite game is %s\n", game);
+
+    return 0;
+}
